473fcc1cdf717213d558618b6cfa6fec657402af
John Adrian Ticatic <john.ticatic@gmail.com>
2024-12-12 16:33:11 +0800

    adding home accounting

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,20 +22,221 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GunaElipse1 = New Guna.UI.WinForms.GunaElipse(Me.components)
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.GunaControlBox3 = New Guna.UI.WinForms.GunaControlBox()
+        Me.GunaControlBox2 = New Guna.UI.WinForms.GunaControlBox()
+        Me.GunaControlBox1 = New Guna.UI.WinForms.GunaControlBox()
+        Me.GunaDragControl1 = New Guna.UI.WinForms.GunaDragControl(Me.components)
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.GunaTransfarantPictureBox1 = New Guna.UI.WinForms.GunaTransfarantPictureBox()
+        Me.paymentBtn = New Guna.UI.WinForms.GunaAdvenceButton()
+        Me.GunaLabel1 = New Guna.UI.WinForms.GunaLabel()
+        Me.posBtn = New Guna.UI.WinForms.GunaAdvenceButton()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        CType(Me.GunaTransfarantPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'GunaElipse1
+        '
+        Me.GunaElipse1.TargetControl = Me
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(41, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(185, Byte), Integer))
+        Me.Panel2.Controls.Add(Me.GunaControlBox3)
+        Me.Panel2.Controls.Add(Me.GunaControlBox2)
+        Me.Panel2.Controls.Add(Me.GunaControlBox1)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(970, 37)
+        Me.Panel2.TabIndex = 2
+        '
+        'GunaControlBox3
+        '
+        Me.GunaControlBox3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaControlBox3.AnimationHoverSpeed = 0.07!
+        Me.GunaControlBox3.AnimationSpeed = 0.03!
+        Me.GunaControlBox3.ControlBoxType = Guna.UI.WinForms.FormControlBoxType.MinimizeBox
+        Me.GunaControlBox3.IconColor = System.Drawing.Color.White
+        Me.GunaControlBox3.IconSize = 15.0!
+        Me.GunaControlBox3.Location = New System.Drawing.Point(863, 3)
+        Me.GunaControlBox3.Name = "GunaControlBox3"
+        Me.GunaControlBox3.OnHoverBackColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.GunaControlBox3.OnHoverIconColor = System.Drawing.Color.FromArgb(CType(CType(41, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(185, Byte), Integer))
+        Me.GunaControlBox3.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaControlBox3.Size = New System.Drawing.Size(30, 30)
+        Me.GunaControlBox3.TabIndex = 4
+        '
+        'GunaControlBox2
+        '
+        Me.GunaControlBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaControlBox2.AnimationHoverSpeed = 0.07!
+        Me.GunaControlBox2.AnimationSpeed = 0.03!
+        Me.GunaControlBox2.ControlBoxType = Guna.UI.WinForms.FormControlBoxType.MaximizeBox
+        Me.GunaControlBox2.Enabled = False
+        Me.GunaControlBox2.IconColor = System.Drawing.Color.White
+        Me.GunaControlBox2.IconSize = 15.0!
+        Me.GunaControlBox2.Location = New System.Drawing.Point(899, 3)
+        Me.GunaControlBox2.Name = "GunaControlBox2"
+        Me.GunaControlBox2.OnHoverBackColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.GunaControlBox2.OnHoverIconColor = System.Drawing.Color.FromArgb(CType(CType(41, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(185, Byte), Integer))
+        Me.GunaControlBox2.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaControlBox2.Size = New System.Drawing.Size(30, 30)
+        Me.GunaControlBox2.TabIndex = 3
+        '
+        'GunaControlBox1
+        '
+        Me.GunaControlBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaControlBox1.AnimationHoverSpeed = 0.07!
+        Me.GunaControlBox1.AnimationSpeed = 0.03!
+        Me.GunaControlBox1.IconColor = System.Drawing.Color.White
+        Me.GunaControlBox1.IconSize = 15.0!
+        Me.GunaControlBox1.Location = New System.Drawing.Point(935, 3)
+        Me.GunaControlBox1.Name = "GunaControlBox1"
+        Me.GunaControlBox1.OnHoverBackColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.GunaControlBox1.OnHoverIconColor = System.Drawing.Color.WhiteSmoke
+        Me.GunaControlBox1.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaControlBox1.Size = New System.Drawing.Size(30, 30)
+        Me.GunaControlBox1.TabIndex = 2
+        '
+        'GunaDragControl1
+        '
+        Me.GunaDragControl1.TargetControl = Me.Panel2
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.Panel3.Controls.Add(Me.GunaTransfarantPictureBox1)
+        Me.Panel3.Controls.Add(Me.paymentBtn)
+        Me.Panel3.Controls.Add(Me.GunaLabel1)
+        Me.Panel3.Controls.Add(Me.posBtn)
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel3.Location = New System.Drawing.Point(0, 37)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(970, 47)
+        Me.Panel3.TabIndex = 6
+        '
+        'GunaTransfarantPictureBox1
+        '
+        Me.GunaTransfarantPictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GunaTransfarantPictureBox1.BaseColor = System.Drawing.Color.Black
+        Me.GunaTransfarantPictureBox1.Image = CType(resources.GetObject("GunaTransfarantPictureBox1.Image"), System.Drawing.Image)
+        Me.GunaTransfarantPictureBox1.Location = New System.Drawing.Point(26, 2)
+        Me.GunaTransfarantPictureBox1.Name = "GunaTransfarantPictureBox1"
+        Me.GunaTransfarantPictureBox1.Size = New System.Drawing.Size(40, 42)
+        Me.GunaTransfarantPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.GunaTransfarantPictureBox1.TabIndex = 6
+        Me.GunaTransfarantPictureBox1.TabStop = False
+        '
+        'paymentBtn
+        '
+        Me.paymentBtn.AnimationHoverSpeed = 0.07!
+        Me.paymentBtn.AnimationSpeed = 0.03!
+        Me.paymentBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.paymentBtn.BaseColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.paymentBtn.BorderColor = System.Drawing.Color.Black
+        Me.paymentBtn.CheckedBaseColor = System.Drawing.Color.Gray
+        Me.paymentBtn.CheckedBorderColor = System.Drawing.Color.Black
+        Me.paymentBtn.CheckedForeColor = System.Drawing.Color.White
+        Me.paymentBtn.CheckedImage = CType(resources.GetObject("paymentBtn.CheckedImage"), System.Drawing.Image)
+        Me.paymentBtn.CheckedLineColor = System.Drawing.Color.DimGray
+        Me.paymentBtn.DialogResult = System.Windows.Forms.DialogResult.None
+        Me.paymentBtn.FocusedColor = System.Drawing.Color.Empty
+        Me.paymentBtn.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.paymentBtn.ForeColor = System.Drawing.Color.FromArgb(CType(CType(41, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(185, Byte), Integer))
+        Me.paymentBtn.Image = CType(resources.GetObject("paymentBtn.Image"), System.Drawing.Image)
+        Me.paymentBtn.ImageSize = New System.Drawing.Size(20, 20)
+        Me.paymentBtn.LineColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.paymentBtn.Location = New System.Drawing.Point(636, 6)
+        Me.paymentBtn.Name = "paymentBtn"
+        Me.paymentBtn.OnHoverBaseColor = System.Drawing.Color.FromArgb(CType(CType(41, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(185, Byte), Integer))
+        Me.paymentBtn.OnHoverBorderColor = System.Drawing.Color.Black
+        Me.paymentBtn.OnHoverForeColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.paymentBtn.OnHoverImage = CType(resources.GetObject("paymentBtn.OnHoverImage"), System.Drawing.Image)
+        Me.paymentBtn.OnHoverLineColor = System.Drawing.Color.FromArgb(CType(CType(41, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(185, Byte), Integer))
+        Me.paymentBtn.OnPressedColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.paymentBtn.Size = New System.Drawing.Size(123, 35)
+        Me.paymentBtn.TabIndex = 7
+        Me.paymentBtn.Text = "PAYMENTS"
+        '
+        'GunaLabel1
+        '
+        Me.GunaLabel1.AutoSize = True
+        Me.GunaLabel1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GunaLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(41, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(185, Byte), Integer))
+        Me.GunaLabel1.Location = New System.Drawing.Point(72, 13)
+        Me.GunaLabel1.Name = "GunaLabel1"
+        Me.GunaLabel1.Size = New System.Drawing.Size(116, 21)
+        Me.GunaLabel1.TabIndex = 6
+        Me.GunaLabel1.Text = "ACCOUNTING"
+        '
+        'posBtn
+        '
+        Me.posBtn.AnimationHoverSpeed = 0.07!
+        Me.posBtn.AnimationSpeed = 0.03!
+        Me.posBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.posBtn.BaseColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.posBtn.BorderColor = System.Drawing.Color.Black
+        Me.posBtn.CheckedBaseColor = System.Drawing.Color.Gray
+        Me.posBtn.CheckedBorderColor = System.Drawing.Color.Black
+        Me.posBtn.CheckedForeColor = System.Drawing.Color.White
+        Me.posBtn.CheckedImage = CType(resources.GetObject("posBtn.CheckedImage"), System.Drawing.Image)
+        Me.posBtn.CheckedLineColor = System.Drawing.Color.DimGray
+        Me.posBtn.DialogResult = System.Windows.Forms.DialogResult.None
+        Me.posBtn.FocusedColor = System.Drawing.Color.Empty
+        Me.posBtn.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.posBtn.ForeColor = System.Drawing.Color.FromArgb(CType(CType(41, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(185, Byte), Integer))
+        Me.posBtn.Image = CType(resources.GetObject("posBtn.Image"), System.Drawing.Image)
+        Me.posBtn.ImageSize = New System.Drawing.Size(20, 20)
+        Me.posBtn.LineColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.posBtn.Location = New System.Drawing.Point(542, 6)
+        Me.posBtn.Name = "posBtn"
+        Me.posBtn.OnHoverBaseColor = System.Drawing.Color.FromArgb(CType(CType(41, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(185, Byte), Integer))
+        Me.posBtn.OnHoverBorderColor = System.Drawing.Color.Black
+        Me.posBtn.OnHoverForeColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.posBtn.OnHoverImage = CType(resources.GetObject("posBtn.OnHoverImage"), System.Drawing.Image)
+        Me.posBtn.OnHoverLineColor = System.Drawing.Color.FromArgb(CType(CType(41, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(185, Byte), Integer))
+        Me.posBtn.OnPressedColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(246, Byte), Integer))
+        Me.posBtn.Size = New System.Drawing.Size(88, 35)
+        Me.posBtn.TabIndex = 6
+        Me.posBtn.Text = "POS"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(970, 596)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Form1"
+        Me.TopMost = True
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        CType(Me.GunaTransfarantPictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents GunaElipse1 As Guna.UI.WinForms.GunaElipse
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents GunaControlBox3 As Guna.UI.WinForms.GunaControlBox
+    Friend WithEvents GunaControlBox2 As Guna.UI.WinForms.GunaControlBox
+    Friend WithEvents GunaControlBox1 As Guna.UI.WinForms.GunaControlBox
+    Friend WithEvents GunaDragControl1 As Guna.UI.WinForms.GunaDragControl
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents GunaTransfarantPictureBox1 As Guna.UI.WinForms.GunaTransfarantPictureBox
+    Friend WithEvents paymentBtn As Guna.UI.WinForms.GunaAdvenceButton
+    Friend WithEvents GunaLabel1 As Guna.UI.WinForms.GunaLabel
+    Friend WithEvents posBtn As Guna.UI.WinForms.GunaAdvenceButton
 End Class
